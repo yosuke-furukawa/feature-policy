130727309fdd6d309fdd1fa5f4c639980073fc9b
Yosuke Furukawa <yosuke.furukawa@gmail.com>
2018-10-14 22:27:12 +0900

    add examples

--- example/app.js
+++ example/app.js
@@ -3,9 +3,12 @@ const featurePolicy = require('../index.js');
 const app = express();
 
 app.use(featurePolicy({
-  "lazyload": "'none'",
-  "document-write": "'none'",
+  // localhost:3000/syncxhr.html
   "sync-xhr": "'none'",
+  // localhost:3000/document-write.html
+  "document-write": "'none'",
+  // localhost:3000/vertical-scroll.html
+  "vertical-scroll": "'none'",
   "legacy-image-formats": "'none'",
 }));
 app.use(express.static('public'));
